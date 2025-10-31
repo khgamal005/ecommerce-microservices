16115a606f088296855831bdd51b266d56cdbc44
kgamal005 <khgamal005@gmail.com>
2025-10-31 03:58:46 +0200

    first commit

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -1,14 +1,29 @@
 import express from 'express';
-
-const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+import cors from "cors";
 
 const app = express();
+
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+
+const host = process.env.HOST ?? 'localhost';
+const port = process.env.PORT ? Number(process.env.PORT) : 6001;
+
 
 app.get('/', (req, res) => {
   res.send({ message: 'Hello API' });
 });
 
 app.listen(port, host, () => {
-  console.log(`[ ready ] http://${host}:${port}`);
+  console.log(`[ ready ] http://${host}:${port}/api`);
 });
+
+Server.on('error', (err) => {
+  console.error('Server error:', err);
+}); 
